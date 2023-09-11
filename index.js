3374547c3a6a70d7220da91c285f289cdacc58ab
Greenblob123 <25YHUAN277C@student.ri.edu.sg>
2023-09-11 17:27:02 +0800

    redid everything again

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const { REST, Routes } = require("discord.js");
-const { clientId, guildId, token } = require("./config.json");
 const fs = require("node:fs");
 const path = require("node:path");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
 
-const commands = [];
-// Grab all the command folders from the commands directory you created earlier
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
 const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-  // Grab all the command files from the commands directory you created earlier
   const commandsPath = path.join(foldersPath, folder);
   const commandFiles = fs
     .readdirSync(commandsPath)
     .filter((file) => file.endsWith(".js"));
-  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
     if ("data" in command && "execute" in command) {
-      commands.push(command.data.toJSON());
+      client.commands.set(command.data.name, command);
     } else {
       console.log(
         `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
@@ -28,27 +27,33 @@ for (const folder of commandFolders) {
   }
 }
 
-// Construct and prepare an instance of the REST module
-const rest = new REST().setToken(token);
+client.once(Events.ClientReady, () => {
+  console.log("Ready!");
+});
 
-// and deploy your commands!
-(async () => {
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
   try {
-    console.log(
-      `Started refreshing ${commands.length} application (/) commands.`
-    );
-
-    // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands }
-    );
-
-    console.log(
-      `Successfully reloaded ${data.length} application (/) commands.`
-    );
+    await command.execute(interaction);
   } catch (error) {
-    // And of course, make sure you catch and log any errors!
     console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
   }
-})();
+});
+
+client.login(token);
