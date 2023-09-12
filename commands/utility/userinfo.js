fcd3a0b2ea29e047e9ece758867a4b6a34cdc0fb
Greenblob123 <25YHUAN277C@student.ri.edu.sg>
2023-09-12 11:33:38 +0800

    increase the amount of functions

--- commands/utility/userinfo.js
+++ commands/utility/userinfo.js
@@ -2,13 +2,13 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("blah")
+    .setName("user")
     .setDescription("Provides information about the user."),
   async execute(interaction) {
     // interaction.user is the object representing the User who ran the command
     // interaction.member is the GuildMember object, which represents the user in the specific guild
     await interaction.reply(
-      `This command was run by ${interaction.user.username}.`
+      `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
     );
   },
 };
