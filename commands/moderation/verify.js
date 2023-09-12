e709bae212e1e4564911ff65ea204dc97904d625
Greenblob123 <25YHUAN277C@student.ri.edu.sg>
2023-09-12 14:52:09 +0800

    succesfully implemented it

---
+++ commands/moderation/verify.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("verify")
+    .setDescription("Lets you be verified.")
+    .addStringOption((option) =>
+      option.setName("name").setDescription("Your name").setRequired(true)
+    ),
+  async execute(interaction) {
+    const client = interaction.client;
+    const name = interaction.options.getString("name");
+    const channel = client.channels.cache.get("1150657346817769560");
+
+    await channel.send(name + " Do you want to verify this person?");
+
+    await interaction.channel.bulkDelete(name, true).catch((error) => {
+      console.error(error);
+      interaction.reply({
+        content: "There was an error transmitting this to the mods",
+        ephemeral: true,
+      });
+    });
+
+    return interaction.reply({
+      content: `Wait for review`,
+      ephemeral: true,
+    });
+  },
+};
