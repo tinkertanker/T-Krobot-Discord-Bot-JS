8bd9916c1faf980e5fe0d8a6fa8dc5cd61ed1917
Greenblob123 <25YHUAN277C@student.ri.edu.sg>
2023-09-21 11:07:36 +0800

    fixed all remaining bugs, verify works

--- commands/moderation/verify.js
+++ commands/moderation/verify.js
@@ -11,7 +11,7 @@ const {
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("verify")
-    .setDescription("Lets you be verified.")
+    .setDescription("Lets you sbe verified.")
     .addStringOption((option) =>
       option.setName("name").setDescription("Your name").setRequired(true)
     ),
@@ -19,12 +19,11 @@ module.exports = {
     const client = interaction.client;
     const name = interaction.options.getString("name");
     const channel = client.channels.cache.get("1086943862796333118");
-    const verifiedRole = "1086943974004113481";
-    const tinkertanker = "1086189634008141824";
+    const trainerRole = "1086943974004113481";
+    const tinkertankerRole = "1086189634008141824";
     const { guild } = interaction;
     const { ViewChannel, ReadMessageHistory, SendMessages } =
       PermissionFlagsBits;
-
     await channel.send({
       embeds: [
         new EmbedBuilder()
@@ -51,86 +50,58 @@ module.exports = {
       ],
     });
     client.on("interactionCreate", async (interaction) => {
+      async function doStuff(role) {
+        //Mods side
+        await interaction.member.roles
+          .add(role)
+          // eslint-disable-next-line no-unused-vars
+          .then((member) =>
+            interaction.reply({
+              content: `User is now Verified `,
+              ephemeral: true,
+            })
+          )
+          .catch((err) => {
+            console.log(err);
+            return interaction.reply({
+              content: "Something went wrong",
+              ephemeral: true,
+            });
+          });
+        //User side
+        await interaction.user.send("You are now verified");
+        //create a personal channel
+        await interaction.guild.channels
+          .create({
+            name: `${name.replace(" ", "-")}`,
+            type: ChannelType.GuildText,
+            parent: "1103947971734810745",
+            permissionOverwrites: [
+              {
+                id: interaction.user.id,
+                allow: [ViewChannel, SendMessages, ReadMessageHistory],
+              },
+              {
+                id: guild.roles.everyone.id,
+                deny: [ViewChannel],
+              },
+            ],
+          })
+          .then((channel) => channel.send("Welcome to your channel!"))
+          .catch((err) => {
+            console.log(err);
+            return interaction.reply({
+              content: "Could not create channel",
+              ephemeral: true,
+            });
+          });
+      }
+
       if (interaction.isButton()) {
         if (interaction.customId == "verified") {
-          //Mods side
-          await interaction.member.roles
-            .add(verifiedRole)
-            // eslint-disable-next-line no-unused-vars
-            .then((member) =>
-              interaction.reply({
-                content: `User is now Trainer `,
-                ephemeral: true,
-              })
-            )
-            .catch((err) => {
-              console.log(err);
-              return interaction.reply({
-                content: "Something went wrong",
-                ephemeral: true,
-              });
-            });
-          //User side (may need to async this)
-          await interaction.user.send("You are now verified");
-          //create a personal channel, still does not work, apparently still connected??????
-          await interaction.guild.channels
-            .create({
-              name: `${name}`,
-              type: ChannelType.GuildText,
-              parent: "1103947971734810745",
-              permissionOverwrites: [
-                {
-                  allow: [ViewChannel, SendMessages, ReadMessageHistory],
-                },
-              ],
-            })
-            .then((channel) => channel.send("Welcome to your channel!"))
-            .catch((err) => {
-              console.log(err);
-              return interaction.reply({
-                content: "Could not create channel",
-                ephemeral: true,
-              });
-            });
+          doStuff(trainerRole);
         } else if (interaction.customId == "tinkertanker") {
-          await interaction.member.roles
-            .add(tinkertanker)
-            // eslint-disable-next-line no-unused-vars
-            .then((member) =>
-              interaction.reply({
-                content: `User is now Tinkertanker `,
-                ephemeral: true,
-              })
-            )
-            .catch((err) => {
-              console.log(err);
-              return interaction.reply({
-                content: "Something went wrong",
-                ephemeral: true,
-              });
-            });
-          //User side (may need to async this)
-          await interaction.user.send("You are now verified");
-          //create a personal channel
-          await interaction.guild.channels
-            .create({
-              name: `${name}`,
-              type: ChannelType.GuildText,
-              parent: "1103947971734810745",
-              permissionOverwrites: [
-                {
-                  allow: [ViewChannel, SendMessages, ReadMessageHistory],
-                },
-              ],
-            })
-            .then((channel) => channel.send("Welcome to your channel!"))
-            .catch((err) => {
-              console.log(err);
-              return interaction.reply({
-                content: "Could not create channel",
-                ephemeral: true,
-              });
-            });
+          doStuff(tinkertankerRole);
         } else {
           //mods side, no need return
           interaction.reply({
