9dca2868c3121af24c7cb8ae08ed2a2320bac516
TwelvePenguins <yuhan.du@gmail.com>
2024-01-06 23:42:49 +0800

    add invite

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -1,27 +1,62 @@
 const {
-    SlashCommandBuilder,
-    PermissionFlagsBits,
-    ChannelType,
-    EmbedBuilder,
-  } = require("discord.js");
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+} = require("discord.js");
 
-  module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("invite")
-        .setDiscription("Invite a member to this active class text channel")
-        .setDefaultMemberPermissions(PermissionFlagsBits.administrator)
-        .addUserOption(option =>
-          option.setName("user")
-            .setRequired(true)),
-    async execute(interaction) {
-      const { options } = interaction;
-      const user = options.getUser("user");
+module.exports = {
+  data: new SlashCommandBuilder()
+      .setName("invite")
+      .setDescription("Invite up to 5 members to a active class text channel")
+      .setDefaultMemberPermissions(PermissionFlagsBits.administrator)
+      .addChannelOption(option => 
+        option 
+          .setName("channel")
+          .setDescription("The channel that you wish to add users to")
+          .setRequired(true))
+      .addUserOption(option =>
+        option
+          .setName("user1")
+          .setDescription("The user that you wish to add")
+          .setRequired(true))
+      .addUserOption(option =>
+        option
+          .setName("user2")
+          .setDescription("The user that you wish to add"))
+      .addUserOption(option =>
+        option
+          .setName("user3")
+          .setDescription("The user that you wish to add"))
+      .addUserOption(option =>
+        option
+          .setName("user4")
+          .setDescription("The user that you wish to add"))
+      .addUserOption(option =>
+        option
+          .setName("user5")
+          .setDescription("The user that you wish to add")),
 
-      try {
-        await ChannelType.permissionOverwrites.edit(option, {ViewChannel: true});
-      } catch {
-        console.error(error);
-        console.log(user)
+  async execute(interaction) {
+    const { options } = interaction;
+    const targetChannel = options.getChannel("channel");
+    let users = [];
+    
+    for (let i = 1; i <= 5; i++) {
+      users.push(options.getUser("user" + i.toString()) ?? "No user");
+    };
+
+    try {
+      await interaction.reply({ content: "Adding members...", ephemeral: true });
+      for (let x in users) {
+        if (users[x] !== "No user") {
+          console.log(users[x])
+          await targetChannel.permissionOverwrites.edit(users[x].id, { ViewChannel: true });
+          await interaction.editReply({ content: `${users[x].username} has been added.`, ephemeral: true });
+        } else {
+          console.log(`No user recieved ${users[x].username}`);
+        }
       }
+    } catch(error) {
+      console.error(error);
     }
   }
+}
