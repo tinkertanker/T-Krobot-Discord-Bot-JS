256029b2a399251b51901f37807b478527434128
TwelvePenguins <yuhan.du@gmail.com>
2024-01-19 13:11:03 +0800

    add embed to CFI

--- commands/utility/call-instructors.js
+++ commands/utility/call-instructors.js
@@ -3,6 +3,7 @@ const {
     PermissionFlagsBits,
     bold, 
     underscore,
+    EmbedBuilder,
 } = require("discord.js");
 
 module.exports = {
@@ -19,7 +20,7 @@ module.exports = {
         .addStringOption( option => 
             option
                 .setName("when")
-                .setDescription("Add the date and time of the class")
+                .setDescription("Add the time of the class")
                 .setRequired(true)
             )
         .addStringOption( option => 
@@ -36,48 +37,62 @@ module.exports = {
             )
         .addStringOption( option => 
             option
-                .setName("details")
+                .setName("what")
                 .setDescription("Add any necessary details")
+                .setRequired(true)
+            )
+        .addStringOption( option => 
+            option 
+                .setName("link")
+                .setDescription("Add any relevant URL. Don't forget the https://")
+            )
+        .addAttachmentOption( option =>
+            option
+                .setName("image")
+                .setDescription("Add any accompanying image reference.")
             ),
 
     async execute(interaction) {
         const { options } = interaction;
         const className = options.getString("title");
-        const classDetails = options.getString("details") ?? "No specific details have been provided.";
-        const classTime = options.getString("when"); 
+        const classDetails = options.getString("what") ?? "No specific details have been provided.";
+        const classTime = options.getString("when").replace(/\\n/, "\n"); 
         const classLocation = options.getString("where");
         const classManpower = options.getString("who"); 
-
-        const reply = await interaction.reply ({
-            content: 
-            `
-${underscore(bold("CFI: " + className))}
-
-${underscore("Details: ")}
-${classDetails}
-
-${underscore("Key Information: ")}
-- When: ${classTime}
-- Where: ${classLocation}
-- No. of instructors needed: ${classManpower}
-
----
-Note: a ✅ indicates that the CFI is open, and a ❌ indicates that the CFI is closed.`,
-          fetchReply: true,
-        });
-              
+        const classURL = options.getString("link");
+        let classImage = options.getAttachment("image") ?? "https://tinkercademy.com/wp-content/uploads/2019/03/01-Tinkercademy-Tertiary-Logo-Colour-On-Light-460w.png";
+        const channel = interaction.channel ?? "Not a text channel";
+      
+        if (typeof(classImage) !== "string") {
+          classImage = classImage.url
+        }
+        
+        const replyEmbed = await new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle(className)
+            .setURL(classURL)
+            .setDescription(classDetails)
+            .addFields(
+                { name: "Where", value: classLocation, inline: true },
+                { name: "Who", value: classManpower, inline: true }, 
+                { name: "When", value: classTime },
+            )
+            .setImage(classImage)
+        
+        const reply = await channel.send({ embeds: [replyEmbed] })
+        
         try {
             await reply.startThread({
-                name: `CFI: ${className}`, 
-                autoArchiveDuration: 1440,
+                  name: `CFI: ${className}`, 
+                  autoArchiveDuration: 1440,
             });
-            await interaction.followUp({ content: "Your thread has been successfully created", ephemeral: true});
+            await interaction.reply({ content: "Your thread has been successfully created", ephemeral: true});
             await reply.react("✅");
         } catch(error) {
             console.error(error);
             interaction.followUp({
-              content: "An error occurred while creating the accompanying thread.",
-              ephemeral: true,
+                content: "An error occurred while creating the accompanying thread.",
+                ephemeral: true,
             });
         }
     }
